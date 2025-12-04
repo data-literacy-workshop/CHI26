a7ef4105274963df1852f14dea2b91bcd65e7ab5
Lily Ge <lilyge@umich.edu>
2025-12-03 21:03:22 -0600

    update

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'CHI 2026';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Data Literacy for the 21st Century: Perspectives from Visualization, Cognitive Science, Artificial Intelligence, and Education';
+export const SITE_DESCRIPTION = 'CHI 2026 Workshop';
